e065879236faa8cab8c1ac6d87ee0cf36cec697a
hongmaoxiao <769501294@qq.com>
2017-11-19 16:33:24 +0800

    add isNow

--- test/beginningOfDay.test.js
+++ test/beginningOfDay.test.js
@@ -4,3 +4,9 @@ test('return beginningOfDay default', () => {
   const now = new Now(2017, 9, 29, 17, 35, 20, 100);
   expect(now.beginningOfDay()).toBe('2017-10-29 00:00:00');
 });
+
+test('return Now instance by `self` argument', () => {
+  const now = new Now(2017, 9, 29, 17, 35, 20, 100);
+  const newNow = now.beginningOfDay('self');
+  expect(newNow.isNow()).toBeTruthy();
+});
